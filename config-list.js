114f53d48964db270a20763f40104fbfacf540b5
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-01-22 01:27:55 +0800

    西元2024年01月22日 (週一) 01時27分55秒 CST

--- config-list.js
+++ config-list.js
@@ -5,7 +5,7 @@ let feedList = [
   { 
     title: 'MOOC 分類編目（113年基礎）',
     feedID: 'MOOC_Classification_Cataloging',
-    homepageURL: [{"title":"學校圖書館的分類編目基本原則和應用-影片 ewantvideo","url":"https://www.youtube.com/watch?v=Nan7f398sXI"},{"title":"中文圖書分類編目的規則和實例-影片 ewantvideo","url":"https://www.youtube.com/watch?v=LGslaDP1rwk"},{"title":"館藏資料的加工、排架、淘汰和報廢-影片 ewantvideo","url":"https://www.youtube.com/watch?v=JG6IEU0PGt4"},{"title":"館藏資料的統計分析和應用-影片 ewantvideo","url":"https://www.youtube.com/watch?v=b8Hc8Nvf7cA"}],
+    homepageURL: [{"title":"1. 學校圖書館的分類編目基本原則和應用-影片 ewantvideo","url":"https://www.youtube.com/watch?v=Nan7f398sXI"},{"title":"2. 中文圖書分類編目的規則和實例-影片 ewantvideo","url":"https://www.youtube.com/watch?v=LGslaDP1rwk"},{"title":"3. 館藏資料的加工、排架、淘汰和報廢-影片 ewantvideo","url":"https://www.youtube.com/watch?v=JG6IEU0PGt4"},{"title":"4. 館藏資料的統計分析和應用-影片 ewantvideo","url":"https://www.youtube.com/watch?v=b8Hc8Nvf7cA"}],
     itemFilters: [],
   },
 ]
@@ -14,11 +14,11 @@ let feedList = [
 
 if (CONFIG.debug) {
   feedList = [
-    {
-      title: '小宁子 3C',
-      feedID: 'xnzxnz',
-      homepageURL: 'https://www.youtube.com/channel/UCvUJ6BwgUGWBHuUd0cv546g',
-      itemFilters: ItemFiltersPreset.between3minTo30Min,
+    { 
+      title: 'MOOC 分類編目（113年基礎）',
+      feedID: 'MOOC_Classification_Cataloging',
+      homepageURL: [{"title":"1. 學校圖書館的分類編目基本原則和應用-影片 ewantvideo","url":"https://www.youtube.com/watch?v=Nan7f398sXI"},{"title":"2. 中文圖書分類編目的規則和實例-影片 ewantvideo","url":"https://www.youtube.com/watch?v=LGslaDP1rwk"},{"title":"3. 館藏資料的加工、排架、淘汰和報廢-影片 ewantvideo","url":"https://www.youtube.com/watch?v=JG6IEU0PGt4"},{"title":"4. 館藏資料的統計分析和應用-影片 ewantvideo","url":"https://www.youtube.com/watch?v=b8Hc8Nvf7cA"}],
+      itemFilters: [],
     },
   ]
 }
